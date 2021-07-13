333192463718492e9642a5b59e719a0f19453839
Jeannie <jntsao@gmail.com>
2021-07-13 05:00:33 -0700

    connect to mongodb atlas database online

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-//mongoose.connect("mongodb://localhost:27017/mySyfyDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/mySyfyDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(morgan("common"));
